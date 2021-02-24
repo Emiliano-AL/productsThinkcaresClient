2c7bb6acc4914f9e93d56c16205913475d791877
Emiliano-AL <emiliano.0218@gmail.com>
2021-02-23 23:20:45 -0600

    agrego funcionalidad del search

--- src/companies/List.jsx
+++ src/companies/List.jsx
@@ -5,73 +5,97 @@ import { companyService } from '@/_services';
 
 function List({ match }) {
   const { path } = match;
-  const [ companies, setCompanies] = useState(null);
-  
+  const [companies, setCompanies] = useState(null);
+
   useEffect(() => {
     companyService.getAll().then(x => setCompanies(x));
   }, []);
-  
+
   function deleteCompany(id) {
-    if(window.confirm('¿desea eliminar esta empresa?')){
-        setCompanies(companies.map(x => {
-            if (x._id === id) { x.isDeleting = true; }
-            return x;
-        }));
-        companyService.delete(id).then(() => {
-            setCompanies(companies => companies.filter(x => x._id !== id));
-        });
+    if (window.confirm('¿desea eliminar esta empresa?')) {
+      setCompanies(companies.map(x => {
+        if (x._id === id) { x.isDeleting = true; }
+        return x;
+      }));
+      companyService.delete(id).then(() => {
+        setCompanies(companies => companies.filter(x => x._id !== id));
+      });
     }
   }
   
+  function handleChange(event) {
+    if(event.target.value.length >= 3){
+      let term = event.target.value.toLowerCase();
+      //search action
+      let comps = companies.filter( (comp) => {
+        if(comp.name.toLowerCase().includes(term) )
+          return comp;
+      });
+      setCompanies(comps);
+    }
+    if(event.target.value === ""){
+      companyService.getAll().then(x => setCompanies(x));
+    }
+    event.preventDefault();
+  }
+
   return (
     <div>
-        <h1>Empresas</h1>
-        <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Agregar Empresa</Link>
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th style={{ width: '30%' }}>Nombre</th>
-                    <th style={{ width: '30%' }}>Descripción</th>
-                    <th style={{ width: '10%' }}></th>
-                </tr>
-            </thead>
-            <tbody>
-                {companies && companies.map(comp =>
-                    <tr key={comp._id}>
-                        <td>{comp.name} </td>
-                        <td>{comp.description}</td>
-                        <td style={{ whiteSpace: 'nowrap' }}>
-                            <Link to={`${path}/edit/${comp._id}`} 
-                              className="btn btn-sm btn-primary mr-1">Editar
+       <nav className="navbar navbar-light bg-light">
+        <div className="col-6">
+          <h1>Empresas</h1>
+          <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Agregar Empresa</Link>
+        </div>
+        <div className="form-inline col-6">
+          <input type="search" placeholder="Buscar..." className=" w-100 form-control mr-sm-4" onChange={handleChange} />
+        </div>
+      </nav>
+      
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th style={{ width: '30%' }}>Nombre</th>
+            <th style={{ width: '30%' }}>Descripción</th>
+            <th style={{ width: '10%' }}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {companies && companies.map(comp =>
+            <tr key={comp._id}>
+              <td>{comp.name} </td>
+              <td>{comp.description}</td>
+              <td style={{ whiteSpace: 'nowrap' }}>
+                <Link to={`${path}/edit/${comp._id}`}
+                  className="btn btn-sm btn-primary mr-1">Editar
                             </Link>
-                            <button 
-                              onClick={() => deleteCompany(comp._id)} 
-                              className="btn btn-sm btn-danger btn-delete-user" disabled={comp.isDeleting}>
-                                {
-                                comp.isDeleting 
-                                    ? <span className="spinner-border spinner-border-sm"></span>
-                                    : <span>Eliminar</span>
-                                }
-                            </button>
-                        </td>
-                    </tr>
-                )}
-                {!companies &&
-                    <tr>
-                        <td colSpan="4" className="text-center">
-                            <div className="spinner-border spinner-border-lg align-center"></div>
-                        </td>
-                    </tr>
-                }
-                {companies && !companies.length &&
-                    <tr>
-                        <td colSpan="4" className="text-center">
-                            <div className="p-2">No hay empresas para mostar</div>
-                        </td>
-                    </tr>
-                }
-            </tbody>
-        </table>
+                <button
+                  onClick={() => deleteCompany(comp._id)}
+                  className="btn btn-sm btn-danger btn-delete-user" disabled={comp.isDeleting}>
+                  {
+                    comp.isDeleting
+                      ? <span className="spinner-border spinner-border-sm"></span>
+                      : <span>Eliminar</span>
+                  }
+                </button>
+              </td>
+            </tr>
+          )}
+          {!companies &&
+            <tr>
+              <td colSpan="4" className="text-center">
+                <div className="spinner-border spinner-border-lg align-center"></div>
+              </td>
+            </tr>
+          }
+          {companies && !companies.length &&
+            <tr>
+              <td colSpan="4" className="text-center">
+                <div className="p-2">No hay empresas para mostar</div>
+              </td>
+            </tr>
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/products/List.jsx
+++ src/products/List.jsx
@@ -5,81 +5,107 @@ import { productService } from '@/_services';
 
 function List({ match }) {
   const { path } = match;
-  const [ products, setProducts] = useState(null);
-  
+  const [products, setProducts] = useState(null);
+
   useEffect(() => {
     productService.getAll().then(x => setProducts(x));
   }, []);
-  
+
   function deleteProduct(id) {
-    if(window.confirm('¿desea eliminar este producto?')){
-        setProducts(products.map(x => {
-            if (x._id === id) { x.isDeleting = true; }
-            return x;
-        }));
-        productService.delete(id).then(() => {
-            let prods =  products.filter(x => x._id !== id);
-            setProducts(prods);
-        }); 
+    if (window.confirm('¿desea eliminar este producto?')) {
+      setProducts(products.map(x => {
+        if (x._id === id) { x.isDeleting = true; }
+        return x;
+      }));
+      productService.delete(id).then(() => {
+        let prods = products.filter(x => x._id !== id);
+        setProducts(prods);
+      });
     }
   }
   
+  function handleChange(event) {
+    console.log('val: ', event.target.value);
+    if(event.target.value.length >= 3){
+      let term = event.target.value.toLowerCase();
+      //search action
+      let prods = products.filter( (prod) => {
+        if(prod.title.toLowerCase().includes(term) )
+          return prod;
+      });
+      console.log(prods);
+      setProducts(prods);
+    }
+    if(event.target.value === ""){
+      console.log('hola ');
+      productService.getAll().then(x => setProducts(x));
+    }
+    event.preventDefault();
+  }
+
   return (
     <div>
-        <h1>Productos</h1>
-        <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Agregar Producto</Link>
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th style={{ width: '15%' }}>Nombre</th>
-                    <th style={{ width: '15%' }}>SKU</th>
-                    <th style={{ width: '15%' }}>Estatus</th>
-                    <th style={{ width: '25%' }}>Descripción</th>
-                    <th style={{ width: '15%' }}>Empresa</th>
-                    <th style={{ width: '15%' }}></th>
-                </tr>
-            </thead>
-            <tbody>
-                {products && products.map(prod =>
-                    <tr key={prod._id}>
-                        <td>{prod.title} </td>
-                        <td>{prod.sku}</td>
-                        <td>{ prod.active ? 'Activo' : 'Inactivo'}</td>
-                        <td>{prod.description}</td>
-                        <td>{prod.company?.name}</td>
-                        <td style={{ whiteSpace: 'nowrap' }}>
-                            <Link to={`${path}/edit/${prod._id}`} 
-                              className="btn btn-sm btn-primary mr-1">Editar
-                            </Link>
-                            <button 
-                              onClick={() => deleteProduct(prod._id)} 
-                              className="btn btn-sm btn-danger btn-delete-user" disabled={prod.isDeleting}>
-                                {
-                                prod.isDeleting 
-                                    ? <span className="spinner-border spinner-border-sm"></span>
-                                    : <span>Eliminar</span>
-                                }
-                            </button>
-                        </td>
-                    </tr>
-                )}
-                {!products &&
-                    <tr>
-                        <td colSpan="6" className="text-center">
-                            <div className="spinner-border spinner-border-lg align-center"></div>
-                        </td>
-                    </tr>
-                }
-                {products && !products.length &&
-                    <tr>
-                        <td colSpan="6" className="text-center">
-                            <div className="p-2">No hay productos para mostar</div>
-                        </td>
-                    </tr>
-                }
-            </tbody>
-        </table>
-    </div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="col-6">
+          <h1>Productos</h1>
+          <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Agregar Producto</Link>
+        </div>
+        <div className="form-inline col-6">
+          <input type="search" placeholder="Buscar..." className=" w-100 form-control mr-sm-4" onChange={handleChange} />
+        </div>
+      </nav>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th style={{ width: '15%' }}>Nombre</th>
+            <th style={{ width: '15%' }}>SKU</th>
+            <th style={{ width: '15%' }}>Estatus</th>
+            <th style={{ width: '25%' }}>Descripción</th>
+            <th style={{ width: '15%' }}>Empresa</th>
+            <th style={{ width: '15%' }}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {products && products.map(prod =>
+            <tr key={prod._id}>
+              <td>{prod.title} </td>
+              <td>{prod.sku}</td>
+              <td>{prod.active ? 'Activo' : 'Inactivo'}</td>
+              <td>{prod.description}</td>
+              <td>{prod.company?.name}</td>
+              <td style={{ whiteSpace: 'nowrap' }}>
+                  <Link to={`${path}/edit/${prod._id}`}
+                      className="btn btn-sm btn-primary mr-1">Editar
+              </Link>
+                <button
+                  onClick={() => deleteProduct(prod._id)}
+                  className="btn btn-sm btn-danger btn-delete-user" disabled={prod.isDeleting}>
+                  {
+                      prod.isDeleting
+                          ? <span className="spinner-border spinner-border-sm"></span>
+                          : <span>Eliminar</span>
+                  }
+                </button>
+              </td>
+            </tr>
+          )}
+          {!products &&
+            <tr>
+              <td colSpan="6" className="text-center">
+                <div className="spinner-border spinner-border-lg align-center"></div>
+              </td>
+            </tr>
+          }
+          {products && !products.length &&
+            <tr>
+              <td colSpan="6" className="text-center">
+                <div className="p-2">No hay productos para mostar</div>
+              </td>
+            </tr>
+          }
+      </tbody>
+    </table>
+  </div>
   );
 }
 
